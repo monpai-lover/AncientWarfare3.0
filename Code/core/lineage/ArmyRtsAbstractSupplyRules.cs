b60e0f5e66b9807fa2bb4a1b1ce02820350c4877
monpai-lover <monpai-lover@users.noreply.github.com>
2026-08-13 19:49:03 +0800

    feat: consume RTS rations from army anchor city

--- Code/core/lineage/ArmyRtsAbstractSupplyRules.cs
+++ Code/core/lineage/ArmyRtsAbstractSupplyRules.cs
@@ -14,5 +14,11 @@ namespace AncientWarfare3.core.lineage
         {
             return supplied;
         }
+
+        public static bool CanConsume(bool eligible,
+            bool hasSuitableFood, bool hasFoodItem)
+        {
+            return eligible && hasSuitableFood && hasFoodItem;
+        }
     }
 }
