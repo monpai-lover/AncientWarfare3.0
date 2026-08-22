2402ca6f6b2e70e865e16f8eb5eee971ab09cfdd
monpai-lover <monpai-lover@users.noreply.github.com>
2026-08-22 16:34:04 +0800

    fix: account front lane in render deadline

--- Code/patch/AW_FramePrioritySchedulerPatch.cs
+++ Code/patch/AW_FramePrioritySchedulerPatch.cs
@@ -69,6 +69,7 @@ namespace AncientWarfare3.patch
             {
                 AWCooperativeSimulationRunner runner =
                     AWCooperativeSimulationRunner.Instance;
+                AWFramePriorityGovernor.BeginFrame();
                 bool replicaSession =
                     AW3MultiplayerReplicaScope.IsReplicaSession;
                 if (runner.RequiresControl &&
